5023bedc34089e3fe7811e0ff2238e346cd57b01
lastavengers <lastavengers@outlook.com>
2015-11-28 19:36:24 +0800

    structure of thread & reply

---
+++ discuz_struct.py
@@ -0,0 +1,45 @@
+import re
+
+class Reply():
+    author = ''
+    time = ''
+    message = ''
+
+    def __init__(self, author, time, message): 
+        self.author = author
+        self.time = time
+        self.message = message
+
+class Post():
+    tid = ''
+    author = ''
+    time = ''
+    subject = ''
+    message = ''
+    replies = []
+    num_replies = 0
+
+    def __init__(self): 
+        pass
+
+    def display(self, display_relpy = False, pure = False):
+        print('TID:', self.tid, 'Author:', self.author, 'Time:', self.time)
+        print('Subject:', self.subject)
+        print('Replies number:', self.num_replies)
+        print('---------- Message ----------')
+        if pure:
+            dr = re.compile(r'<[^>]+>', re.S)
+            print(dr.sub('', self.message))
+        else:
+            print(self.message)
+
+        if display_relpy:
+            for r in self.replies:
+                print('-----------------------------')
+                print(r.author, 'reply at', r.time)
+                if pure:
+                    print(dr.sub('', r.message))
+                else:
+                    print(r.message)
+
+        print('----------   End   ----------')
