61751f255b51e182870b83b535dbb11c67459db7
lastavengers <lastavengers@outlook.com>
2016-01-03 00:28:45 +0800

    dzstruct.py: thread can to convert to jsonstring now

--- dzstruct.py
+++ dzstruct.py
@@ -1,3 +1,7 @@
+# -*- encoding: UTF-8 -*-
+
+import json
+
 class Post():
     pid = ''
     uid = ''
@@ -27,10 +31,10 @@ class Thread():
         self.title = title
         self.nposts = nreplies;
 
-    def disp(self):
-        print('TID:', self.tid, 'FID', self.fid, 'Author:', self.posts[0].author, 'Time:', self.posts[0].time)
-        print('Title:', self.title)
-        print('Replies number:', self.nposts)
+    def to_text(self):
+        print('TID:', self.tid, 'FID:', self.fid, '作者:', self.posts[0].author, '发表时间:', self.posts[0].time)
+        print('标题:', self.title)
+        print('回复数:', self.nposts)
         print('---------- Message ----------')
         print(self.posts[0].message)
 
@@ -40,3 +44,28 @@ class Thread():
             print(p.message)
 
         print('----------   End   ----------')
+
+    def to_json(self):
+        thdict = {  'tid': self.tid,
+                    'fid': self.fid,
+                    'title': self.title,
+                    'npost': self.nposts,
+                    'posts': {}
+                    }
+        i = 0
+        for p in self.posts:
+            podict = {  'pid': p.pid,
+                        'uid': p.uid,
+                        'author': p.author,
+                        'time': p.time,
+                        'title': p.title,
+                        'message': p.message
+                        }
+            thdict['posts'][i] = podict
+            i = i + 1
+        thjson = json.dumps(thdict, sort_keys = True,
+                            indent = 4,
+                            separators = (',', ': '),
+                            ensure_ascii = False
+                            )
+        return thjson
